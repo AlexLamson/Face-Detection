2261e852c998cd17042127bd23dc2a1729e32766
Alex Lamson <alex.lamson@gmail.com>
2015-05-12 16:04:56 -0400

    Made Haar.scala more readable

--- src/detector/Haar.scala
+++ src/detector/Haar.scala
@@ -1,25 +1,4 @@
-/*
- * TODO
- * 
- * add toFileString/fromFileString methods
- *   Format:
- *   hasHorzStripes hasTwoStripes whiteFirst x y width height threshold
- *   H 2 W 0 0 10 10 0.54124
- * 
- */
-
 package detector
-
-sealed class Rect(val x:Int, val y:Int, val width:Int, val height:Int){
-  override def toString = s"($x, $y, $width, $height)"
-  override def equals(that:Any) = {
-    that match {
-      case that:Rect => that.x==x && that.y==y &&
-        that.width==width && that.height==height
-      case _ => false
-    }
-  }
-}
 
 //generic haar-like feature
 abstract class Haar(val r:Rect, 
@@ -178,52 +157,13 @@ object Haar {
   }
 }
 
-//haar-like feature with 2 horizontal stripes
-class HaarH2(r:Rect,
-    whiteFirst:Boolean = true, threshold:Int = 0, weight:Double = 0.5)
-    extends Haar(r, true, true, whiteFirst, threshold, weight) {
-  override def calcDifference(img:IntegralImage, dx:Int, dy:Int) = {
-    val A = img.sumRegion(r.x+dx, r.y+dy, r.width, r.height/2)
-    val B = img.sumRegion(r.x+dx, r.y+r.height/2+dy, r.width, r.height/2)
-    val diff = if(whiteFirst) A - B else B - A
-    diff
-  }
-}
-
-//haar-like feature with 2 vertical stripes
-class HaarV2(r:Rect,
-    whiteFirst:Boolean = true, threshold:Int = 0, weight:Double = 0.5)
-    extends Haar(r, false, true, whiteFirst, threshold, weight) {
-  override def calcDifference(img:IntegralImage, dx:Int, dy:Int) = {
-    val A = img.sumRegion(r.x+dx, r.y+dy, r.width/2, r.height)
-    val B = img.sumRegion(r.x+r.width/2+dx, r.y+dy, r.width/2, r.height)
-    val diff = if(whiteFirst) A - B else B - A
-    diff
-  }
-}
-
-//haar-like feature with 3 horizontal stripes
-class HaarH3(r:Rect,
-    whiteFirst:Boolean = true, threshold:Int = 0, weight:Double = 0.5)
-    extends Haar(r, true, false, whiteFirst, threshold, weight) {
-  override def calcDifference(img:IntegralImage, dx:Int, dy:Int) = {
-    val A = img.sumRegion(r.x+dx, r.y+dy, r.width, r.height/3) + 
-      img.sumRegion(r.x+dx, r.y+2*r.height/3+dy, r.width, r.height/3)
-    val B = img.sumRegion(r.x+dx, r.y+r.height/3+dy, r.width, r.height/3)
-    val diff = if(whiteFirst) A - B else B - A
-    diff
-  }
-}
-
-//haar-like feature with 3 vertical stripes
-class HaarV3(r:Rect,
-    whiteFirst:Boolean = true, threshold:Int = 0, weight:Double = 0.5)
-    extends Haar(r, false, false, whiteFirst, threshold, weight) {
-  override def calcDifference(img:IntegralImage, dx:Int, dy:Int) = {
-    val A = img.sumRegion(r.x+dx, r.y+dy, r.width/3, r.height) + 
-      img.sumRegion(r.x+2*r.width/3+dx, r.y+dy, r.width/3, r.height)
-    val B = img.sumRegion(r.x+r.width/3+dx, r.y+dy, r.width/3, r.height)
-    val diff = if(whiteFirst) A - B else B - A
-    diff
+sealed class Rect(val x:Int, val y:Int, val width:Int, val height:Int){
+  override def toString = s"($x, $y, $width, $height)"
+  override def equals(that:Any) = {
+    that match {
+      case that:Rect => that.x==x && that.y==y &&
+        that.width==width && that.height==height
+      case _ => false
+    }
   }
 }
