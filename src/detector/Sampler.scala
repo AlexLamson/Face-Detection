54f489a96ea0f0d8b42437c885d9f2564a9c34cb
Alex Lamson <alex.lamson@gmail.com>
2015-05-12 18:36:35 -0400

    Added Detector class

--- src/detector/Sampler.scala
+++ src/detector/Sampler.scala
@@ -1,8 +1,5 @@
 package detector
 
 object Sampler extends App {
-//  for each scale (multiply by 1.25 at each step)
-//  for each block of pixels in the image
-//    if all haar-like features are positive, mark the block
-//    otherwise move to the next block
+
 }
