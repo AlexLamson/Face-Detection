a10a54fc01d98f429bd6c6708e41da157e9aac9b
Alex Lamson <alex.lamson@gmail.com>
2015-05-12 18:07:22 -0400

    Removed comment outline in Tester

--- src/detector/Tester.scala
+++ src/detector/Tester.scala
@@ -76,11 +76,4 @@ object Tester extends App {
   
   val endTime = System.currentTimeMillis
   println("program finished in "+(endTime-startTime)+" ms")
-  
-//  for each block of pixels in the image
-//    for each haar-like feature (in the now sorted order)
-//      run the haar-like feature on the image
-//      if it is positive and the last feature, mark the block
-//      if it is positive and not the last feature, continue
-//      if it is negative, move to the next block
 }
