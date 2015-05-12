87fdc12842f515f81b2d7efe5e782943ccb8a919
Alex Lamson <alex.lamson@gmail.com>
2015-05-11 23:16:50 -0400

    Added notes to Tester

--- src/detector/Tester.scala
+++ src/detector/Tester.scala
@@ -1,5 +1,10 @@
 package detector
 
 object Tester extends App {
-
+//  for each block of pixels in the image
+//    for each haar-like feature (in the now sorted order)
+//      run the haar-like feature on the image
+//      if it is positive and the last feature, mark the block
+//      if it is positive and not the last feature, continue
+//      if it is negative, move to the next block
 }
