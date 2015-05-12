2a2cae22150cfad2175492d9a069f0f8961300d3
Alex Lamson <alex.lamson@gmail.com>
2015-05-12 12:13:07 -0400

    Added threshold improvement methods
    + flip the black & white regions as necessary
    + update the weight according to error %

--- src/detector/Haar.scala
+++ src/detector/Haar.scala
@@ -18,9 +18,10 @@ sealed class Rect(val x:Int, val y:Int, val width:Int, val height:Int){
 abstract class Haar(val r:Rect, 
     val hasHorzStripes:Boolean, 
     val hasTwoStripes:Boolean,
-    val whiteFirst:Boolean = true){
+    var whiteFirst:Boolean = true){
   
   var threshold = 0.5
+  var weight = 0.5
   
   //white area minus black area
   def calcDifference(img:IntegralImage, dx:Int, dy:Int):Int
@@ -30,8 +31,68 @@ abstract class Haar(val r:Rect,
   def isFaceFeature(img:IntegralImage):Boolean = 
     isFaceFeature(calcDifference(img))
   
+  def update(faceList:List[Int], nonFaceList:List[Int]):Unit = {
+    val (threshold, polarity, percentErrors) = Haar.getThresholdAndPolarityAndError(faceList, nonFaceList)
+    
+    this.threshold = threshold
+    
+    if(polarity == -1) whiteFirst = !whiteFirst
+    
+    weight = 1.0-percentErrors //not totally sure about this part
+  }
 }
 object Haar {
+  
+  //given diff values (made by a single haar-like feature) for faces and non-faces, 
+  //  determine a threshold to separate the two
+  //note that if the polarity is -1, then the white and black regions of 
+  //  the haar-like feature should be swapped
+  //returns (threshold, polarity, percentage of errors)
+  def getThresholdAndPolarityAndError(faceList:List[Int], nonFaceList:List[Int]):(Int, Int, Double) = {
+    val faceAvg = 1.0*faceList.sum/faceList.length
+    val nonFaceAvg = 1.0*nonFaceList.sum/nonFaceList.length
+    
+    //polarity will be 1 if face values more positive than non-face values on average
+    val polarity = if(faceAvg > nonFaceAvg) 1 else -1
+    
+    def getThreshold(negList:List[Int], posList:List[Int]) = {
+      /*
+       * Explanation
+       * + = face value, - = non-face value
+       * - --- - -+-- +- + +++ +++
+       * 
+       * The idea is that the face and non-face values are clustered in
+       * two regions, with some overlap between the two clusters.
+       * Each cluster has a "tail" of values that intersect the other cluster.
+       * The threshold is the average of the medians of the two tails.
+       * 
+       */
+      (posList.min + negList.max)/2
+      
+      val posTail = posList.filter( _ < negList.max )
+      val posMedian = posTail.sorted.apply(posTail.length/2) //note that sorting will be somewhat slow when scaled up
+      
+      val negTail = negList.filter( _ > posList.min )
+      val negMedian = negTail.sorted.apply(negTail.length/2) //note that sorting will be somewhat slow when scaled up
+      
+      (posMedian + negMedian)/2
+    }
+    val threshold = if(polarity == 1) getThreshold(nonFaceList, faceList) else getThreshold(faceList, nonFaceList)
+//    println("threshold: "+threshold)
+    
+    val falseNegatives = faceList.filter(_*polarity < threshold*polarity).length
+    val falsePositives = nonFaceList.filter(_*polarity > threshold*polarity).length
+    val errors = falseNegatives + falsePositives
+    val percentErrors = 1.0*errors/(faceList.length + nonFaceList.length)
+//    val nonErrors = faceList.length+nonFaceList.length-errors
+//    println("false negatives: "+falseNegatives)
+//    println("false positives: "+falsePositives)
+//    println("errors: "+errors)
+//    println("non-errors: "+nonErrors)
+    
+    (threshold, polarity, percentErrors)
+  }
+  
   def randomHaar(width:Int, height:Int):Haar = {
     
     //generate random int in the range [min, max]
