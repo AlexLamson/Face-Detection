5122c1175be41bea7d16d727fbfae875a04d83d3
Alex Lamson <alex.lamson@gmail.com>
2015-07-04 11:19:38 -0700

    Added detector call to Sampler

--- src/detector/Sampler.scala
+++ src/detector/Sampler.scala
@@ -1,5 +1,24 @@
+/*
+ * TODO
+ * 
+ * Add scaling
+ */
+
 package detector
 
-object Sampler extends App {
+import file._
 
+object Sampler extends App {
+  println("sampler starting")
+  val startTime = System.currentTimeMillis
+
+//  val file = "res/samples/cropped_face_small.jpg"
+  val file = "res/samples/face00010.png"
+  
+  val detector = new Detector(File.readImage(file))
+  val marked = detector.markedImage
+  marked.toFile("res/output/markedFaces")
+  
+  val endTime = System.currentTimeMillis
+  println("program finished in "+(endTime-startTime)+" ms")
 }
